4e8771a892d085cd2176b949a6a0cf6ca5306f2f
Ivica Ceraj <ivan@bugaco.com>
2013-04-30 16:25:27 -0400

    Updated command

--- edx_exp_enh.sh
+++ edx_exp_enh.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+vagrant up
 vagrant ssh -c "echo . /home/vagrant/mitx_all/STARTUP >> .bash_login"
 vagrant ssh -c "curl https://raw.github.com/starteam/starteam-linked-samples/master/preview_server.tar.gz | tar xzvpf - ; . ./mitx_all/STARTUP ; cd mitx_all/mitx/ ; ./RESTART-GUNICORN-CMS"
 curl https://raw.github.com/starteam/starteam-linked-samples/master/edx_names.sh |bash
-vagrant ssh -c "mkdir shared; sudo mount -t vboxsf -o uid=1000 vagrant-root /home/vagrant/shared/"
-
+export VAGRANT_SHARE=`vagrant ssh -c "sudo VBoxControl sharedfolder list" | grep ^01 | awk '{print$3}'`
+vagrant ssh -c "mkdir shared; sudo mount -t vboxsf -o uid=1000 $VAGRANT_SHARE /home/vagrant/shared/"
+vagrant ssh -c "ls shared"
